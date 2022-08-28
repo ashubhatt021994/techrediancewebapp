0249233559e3f20b6f6a02eb97a7d115148cfd6e
ashutosh bhatt <1994ashutoshbhatt@gmail.com>
2022-08-28 02:06:08 -0700

    techrediance first time data

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './mycomponent/register/register.component';
+import { LoginComponent } from './mycomponent/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
